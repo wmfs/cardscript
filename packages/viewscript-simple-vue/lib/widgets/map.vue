40597d16107e5363565f6648a204d670e5675132
Tim Needham <tim.needham@wmfs.net>
2018-04-23 22:43:13 +0100

    Updates playpen to include new map widget.

--- packages/viewscript-simple-vue/lib/widgets/map.vue
+++ packages/viewscript-simple-vue/lib/widgets/map.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="form-group">
         <label v-bind:for="widgetId">{{heading}}</label>
-        <iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.openstreetmap.org/export/embed.html?bbox=-1.879638,52.483045,-1.87399,52.484927&amp;layer=mapnik" style="border: 1px solid black"></iframe>
+        <iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=-1.879638,52.483045,-1.87399,52.484927&amp;layer=mapnik" style="border: 1px solid black"></iframe>
         <small class="form-text text-muted">{{help}}</small>
     </div>
 </template>
