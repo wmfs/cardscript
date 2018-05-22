dd0d7ca8778c8e8ff31ae81e7fe8af909138d4e7
Meena.Hoda <meena.hoda@wmfs.net>
2018-05-22 11:07:33 +0100

    react-jsonschema-form to viewscript - update file upload widget

--- packages/react-jsonschema-form-to-viewscript/lib/widgets/file-upload.js
+++ packages/react-jsonschema-form-to-viewscript/lib/widgets/file-upload.js
@@ -5,7 +5,10 @@ module.exports = class FileUpload {
       type: 'fileUpload',
       attributes: {
         heading: options.schema.title,
-        mandatory: options.mandatory
+        mandatory: options.mandatory,
+        formatRestriction: options.schema.DocumentFormat.value,
+        maxFileSize: `${options.schema.allowedDocumentSize.value}kb`,
+        maxNumberOfFiles: parseInt(options.schema.numberOfDocument.value)
       }
     }
     if (options.conditionalSchema.length > 0) this.widget.showWhen = options.conditionalSchema[0]
