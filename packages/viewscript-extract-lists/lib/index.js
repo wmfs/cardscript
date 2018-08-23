b8a0f0e1fb63819268fd8c8a5e234cafa7e12a02
Meena.Hoda <meena.hoda@wmfs.net>
2018-08-23 11:17:20 +0100

    fix: extract list to output 'label' as well as 'text' to satisfy vuetify and quasar

--- packages/viewscript-extract-lists/lib/index.js
+++ packages/viewscript-extract-lists/lib/index.js
@@ -1,43 +1,37 @@
 const DEFAULT_CANDIDATE_WIDGET_TYPES = ['select', 'checkboxList', 'questionnaire']
 
 module.exports = function extractLists (viewscript, options) {
-  let candidateWidgetTypes
-  if (options && options.hasOwnProperty('candidateWidgetTypes')) {
-    candidateWidgetTypes = options.candidateWidgetTypes
-  } else {
-    candidateWidgetTypes = DEFAULT_CANDIDATE_WIDGET_TYPES
-  }
+  const candidateWidgetTypes = options && options.hasOwnProperty('candidateWidgetTypes')
+    ? options.candidateWidgetTypes
+    : DEFAULT_CANDIDATE_WIDGET_TYPES
 
   const lists = {
     $simpleTitleMaps: {}
   }
 
   if (viewscript && viewscript.hasOwnProperty('widgets')) {
-    viewscript.widgets.forEach(
-      function (widget) {
-        if (candidateWidgetTypes.includes(widget.type) && widget.hasOwnProperty('attributes')) {
-          let titleMap = widget.attributes.titleMap
-          if (titleMap) {
-            const map = {}
-            const list = []
-            titleMap.forEach(
-              function (item) {
-                list.push(
-                  {
-                    text: item.title || item.value,
-                    value: item.value
-                  }
-                )
-                map[item.value] = item.title || item.value
+    viewscript.widgets.forEach(widget => {
+      if (candidateWidgetTypes.includes(widget.type) && widget.hasOwnProperty('attributes')) {
+        let titleMap = widget.attributes.titleMap
+        if (titleMap) {
+          const map = {}
+          const list = []
+          titleMap.forEach(item => {
+            list.push(
+              {
+                text: item.title || item.value,
+                label: item.title || item.value,
+                value: item.value
               }
             )
+            map[item.value] = item.title || item.value
+          })
 
-            lists[widget.id] = list
-            lists.$simpleTitleMaps[widget.id] = map
-          }
+          lists[widget.id] = list
+          lists.$simpleTitleMaps[widget.id] = map
         }
       }
-    )
+    })
   }
   return lists
 }
