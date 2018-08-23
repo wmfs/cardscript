17129a610c6b899bb77c26b04daf314b935cd116
Meena.Hoda <meena.hoda@wmfs.net>
2018-08-23 09:19:13 +0100

    refactor: hr impl

--- packages/viewscript-to-quasar/lib/builders/horizontal-line.js
+++ packages/viewscript-to-quasar/lib/builders/horizontal-line.js
@@ -1,14 +1,8 @@
 const ComponentBuilder = require('./../utils/Component-builder')
 
 module.exports = function horizontalLineConverter (widgetDefinition, options) {
-  /*
   const builder = new ComponentBuilder(widgetDefinition)
   const heading = builder.addTag('hr')
-  heading.addAttribute('class', 'my-5')
-  return builder.compile()
-  */
-  const builder = new ComponentBuilder(widgetDefinition)
-  const pre = builder.addTag('pre')
-  pre.content('// TODO: horizontal line!')
+  heading.addAttribute('class', 'q-my-xl')
   return builder.compile()
 }
