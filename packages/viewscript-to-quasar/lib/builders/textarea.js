6fa73bcc5c527d25de2c2b2b96322be80cf8fae6
Meena.Hoda <meena.hoda@wmfs.net>
2018-08-23 11:07:09 +0100

    fix: quasar textarea impl

--- packages/viewscript-to-quasar/lib/builders/textarea.js
+++ packages/viewscript-to-quasar/lib/builders/textarea.js
@@ -1,32 +1,12 @@
 const ComponentBuilder = require('./../utils/Component-builder')
-// const GetAttribute = require('./../utils/Get-attribute')
+const GetAttribute = require('./../utils/Get-attribute')
 
 module.exports = function textareaConverter (widgetDefinition, options) {
-  // default
-  // desc
-  // enabled
-  // heading
-  // help
-  // mandatory
-  // maxCharacters
-  // mincharacters
-  // placeholder
-
-  /*
   const getAttribute = GetAttribute(widgetDefinition)
   const builder = new ComponentBuilder(widgetDefinition)
-  const textField = builder.addTag('v-text-field')
-  textField.addAttribute('id', widgetDefinition.id)
-  textField.bindToModel(widgetDefinition)
-  textField.addAttribute('name', widgetDefinition.id)
-  textField.addAttribute('label', getAttribute('heading'))
-  textField.addAttribute('textarea', null)
-  textField.addAttribute('placeholder', getAttribute('placeholder'))
-  textField.addAttribute('hint', getAttribute('desc'))
-  return builder.compile()
-  */
-  const builder = new ComponentBuilder(widgetDefinition)
-  const pre = builder.addTag('pre')
-  pre.content('// TODO: text area!')
+  const input = builder.addTag('q-input')
+  input.bindToModel(widgetDefinition)
+  input.addAttribute('type', 'textarea')
+  input.addAttribute('float-label', getAttribute('heading'))
   return builder.compile()
 }
