1c65e3330c229b17e221a75b55ea4e7bc3079bd7
Meena.Hoda <meena.hoda@wmfs.net>
2018-08-29 07:59:40 +0100

    added viewscript as public path in config

--- packages/viewscript-quasar-playpen/quasar.conf.js
+++ packages/viewscript-quasar-playpen/quasar.conf.js
@@ -11,6 +11,7 @@ module.exports = function (ctx) {
     ],
     supportIE: false,
     build: {
+      publicPath: 'viewscript',
       scopeHoisting: true,
       vueRouterMode: 'history',
       // vueCompiler: true,
