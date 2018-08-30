a921248eb73c22bd391c373184f1e31ea9a09578
Meena.Hoda <meena.hoda@wmfs.net>
2018-08-30 09:06:54 +0100

    refactor: replace icons with Tymly ones

--- packages/viewscript-quasar-playpen/quasar.conf.js
+++ packages/viewscript-quasar-playpen/quasar.conf.js
@@ -45,27 +45,27 @@ module.exports = function (ctx) {
         theme_color: '#027be3',
         icons: [
           {
-            'src': 'statics/icons/icon-128x128.png',
+            'src': 'statics/icons/tymly-128.png',
             'sizes': '128x128',
             'type': 'image/png'
           },
           {
-            'src': 'statics/icons/icon-192x192.png',
+            'src': 'statics/icons/tymly-192.png',
             'sizes': '192x192',
             'type': 'image/png'
           },
           {
-            'src': 'statics/icons/icon-256x256.png',
+            'src': 'statics/icons/tymly-256.png',
             'sizes': '256x256',
             'type': 'image/png'
           },
           {
-            'src': 'statics/icons/icon-384x384.png',
+            'src': 'statics/icons/tymly-x384.png',
             'sizes': '384x384',
             'type': 'image/png'
           },
           {
-            'src': 'statics/icons/icon-512x512.png',
+            'src': 'statics/icons/tymly-512.png',
             'sizes': '512x512',
             'type': 'image/png'
           }
