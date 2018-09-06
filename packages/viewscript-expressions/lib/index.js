40a0d297b09d426a849186a3f2a5ec5d7c20ddf0
reecebrend <reece.brend@wmfs.net>
2018-09-06 11:32:23 +0100

    refactor(): ViewscriptExpressions is now a class with supporting classes for utility

--- packages/viewscript-expressions/lib/index.js
+++ packages/viewscript-expressions/lib/index.js
@@ -1,15 +1,88 @@
-const getExpressions = require('./viewscript-to-expressions')
-const toAsts = require('./viewscript-to-ast')
-const calculateVisibility = require('./calculate-widget-visibility')
-const clean = require('./clean-viewscript')
+const parse = require('esprima').parse
+const evaluate = require('static-eval')
 
-module.exports = function viewscriptCleaner (viewscript, data) {
-  const expressions = getExpressions(viewscript)
-  const asts = toAsts(expressions)
+class ViewscriptExpressions {
+  constructor () {
+    this.asts = {}
+  }
 
-  const visList = calculateVisibility(viewscript, data, asts)
-  console.log('Visibility List: ', visList)
+  getExpressionsFromViewscript (viewscript) {
+    return viewscript.widgets
+      .filter(widget => widget.showWhen)
+      .map(widget => {
+        return {widget: widget.id, expression: widget.showWhen}
+      })
+  }
 
-  const cleaned = clean(data, visList)
-  return cleaned
+  convertExpressionsToAst (expressions) {
+    /* OUTPUT:
+    asts: {
+      id: ast,
+      id: ast,
+      ...
+    }
+    */
+    expressions.forEach(expression => {
+      const ast = parse(expression.expression)
+      this.asts[expression.widget] = ast
+    })
+  }
+
+  addViewscript (viewscript) {
+    const expressions = this.getExpressionsFromViewscript(viewscript)
+    this.convertExpressionsToAst(expressions)
+  }
+
+  calculateWidgetVisibility (viewscript, data, asts) {
+    let setVisible = [true]
+    const visibilityList = {}
+    let outerVis = true
+
+    viewscript.widgets.forEach(widget => {
+      const currentVis = setVisible[setVisible.length - 1]
+      if (currentVis === false) {
+        // console.log('The set we are in is invisible', widget.id)
+        if (widget.type !== 'endSet') visibilityList[widget.id] = false
+      } else if (widget.showWhen) {
+        const thisAST = asts[widget.id].body[0]
+        const isVis = evaluate(thisAST, {data})
+        if (isVis === false) outerVis = false
+        visibilityList[widget.id] = isVis
+        if (widget.type === 'set') {
+          setVisible.push(isVis)
+        } else if (widget.type === 'endSet') {
+          // console.log('I found the end of the set for ', widget.id)
+          setVisible.pop()
+        } else {
+          // console.log('widget not set')
+        }
+      } else {
+        if (widget.id) {
+          visibilityList[widget.id] = outerVis
+        }
+      }
+    })
+
+    return visibilityList
+  }
+
+  cleanData (data, visibilityList) {
+    // console.log('This is the form data: ', data)
+    // console.log('This is the visibility list: ', visibilityList)
+
+    Object.entries(visibilityList).forEach(condition => {
+      // console.log('evaluating this condition', condition)
+      Object.entries(data).forEach(widget => {
+        // console.log('against this widget', widget)
+        if (condition[0] === widget[0] && condition[1] === false) {
+          // console.log('got a false', widget, condition)
+          delete data[widget[0]]
+        }
+      })
+    })
+    // console.log('data after clean: ', data)
+    return data
+  }
 }
+
+module.exports = ViewscriptExpressions
