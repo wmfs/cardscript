a80ee6822f07e8e55889a8ffec93f28eadd1a32b
Meena.Hoda <meena.hoda@wmfs.net>
2018-12-05 10:39:38 +0000

    Beginning refactor of cardscript-schema into adaptive cards schema utils files and tests need further refactoring - dependant on examples package

--- packages/cardscript-schema/lib/validate-form/formatters/simple.js
+++ packages/cardscript-schema/lib/validate-form/formatters/simple.js
@@ -1,8 +1,14 @@
-const processSchemaErrors = require('./../../utils/process-schema-errors')
+// const processSchemaErrors = require('./../../utils/process-schema-errors')
+
 module.exports = function simpleFormatter (validationResult) {
-  const processedSchemaErrors = processSchemaErrors(validationResult.errors)
+  // const processedSchemaErrors = processSchemaErrors(validationResult.errors)
+  // return {
+  //   widgetsValid: processedSchemaErrors.length === 0,
+  //   errors: processedSchemaErrors
+  // }
+
   return {
-    widgetsValid: processedSchemaErrors.length === 0,
-    errors: processedSchemaErrors
+    widgetsValid: validationResult.errors.length === 0,
+    errors: validationResult.errors
   }
 }
