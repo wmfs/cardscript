c9af452a6c08dc960859ccb93b4a192ee45b3307
Meena.Hoda <meena.hoda@wmfs.net>
2018-12-05 16:04:50 +0000

    refactor extract toc for adaptive cards

--- packages/cardscript-extract-lists/lib/index.js
+++ packages/cardscript-extract-lists/lib/index.js
@@ -44,38 +44,4 @@ module.exports = function (cardscript) {
   }
 
   return lists
-
-  // const candidateWidgetTypes = options && options.hasOwnProperty('candidateWidgetTypes')
-  //   ? options.candidateWidgetTypes
-  //   : DEFAULT_CANDIDATE_WIDGET_TYPES
-  //
-  // const lists = {
-  //   $simpleTitleMaps: {}
-  // }
-  //
-  // if (cardscript && cardscript.hasOwnProperty('widgets')) {
-  //   cardscript.widgets.forEach(widget => {
-  //     if (candidateWidgetTypes.includes(widget.type) && widget.hasOwnProperty('attributes')) {
-  //       let titleMap = widget.attributes.titleMap
-  //       if (titleMap) {
-  //         const map = {}
-  //         const list = []
-  //         titleMap.forEach(item => {
-  //           list.push(
-  //             {
-  //               text: item.title || item.value,
-  //               label: item.title || item.value,
-  //               value: item.value
-  //             }
-  //           )
-  //           map[item.value] = item.title || item.value
-  //         })
-  //
-  //         lists[widget.id] = list
-  //         lists.$simpleTitleMaps[widget.id] = map
-  //       }
-  //     }
-  //   })
-  // }
-  // return lists
 }
--- packages/cardscript-table-of-contents/test/extract-table-of-contents-tests.js
+++ packages/cardscript-table-of-contents/test/extract-table-of-contents-tests.js
@@ -4,27 +4,27 @@
 const extractToc = require('./../lib/')
 const chai = require('chai')
 const expect = chai.expect
-const examples = require('cardscript-examples')
+const { simple, complex } = require('cardscript-examples')
 
 describe('Run some Cardscript table-of-contents-extracting tests', function () {
   it('should extract some defaults from simple Cardscript', function () {
-    const toc = extractToc(examples.simple)
+    const toc = extractToc(simple)
     expect(toc.length).to.equal(0)
   })
 
   it('should extract some defaults from complex Cardscript', function () {
-    const toc = extractToc(examples.complex)
+    const toc = extractToc(complex)
     expect(toc).to.eql(
       [
         {
           tocIcon: 'account_box',
           tocTitle: 'Your Details',
-          widgetId: 'yourDetails'
+          elementId: 'yourDetails'
         },
         {
           tocIcon: 'local_pizza',
           tocTitle: 'The Pizza',
-          widgetId: 'thePizza'
+          elementId: 'thePizza'
         }
       ]
     )
