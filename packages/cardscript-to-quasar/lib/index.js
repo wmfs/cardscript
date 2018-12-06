28bc4e5d410fbf6f296eafe4450eb8fb3ed8ca69
Meena.Hoda <meena.hoda@wmfs.net>
2018-12-06 09:31:34 +0000

    Refactoring cardscript-to-quasar include actions

--- packages/cardscript-to-quasar/lib/index.js
+++ packages/cardscript-to-quasar/lib/index.js
@@ -46,23 +46,11 @@ module.exports = function extractDefaults (cardscript, options) {
     cardscript.body.forEach(parseElement)
   }
 
-  /*
-   if (cardscript.widgets) {
-    cardscript.widgets.forEach(widgetDefinition => {
-      const widgetType = widgetDefinition.type
-      const lines = builders[widgetType].conversionFunction(widgetDefinition, options)
-
-      if (widgetType === 'endSet') {
-        indent = indent.slice(ONE_TAB.length)
-      }
-
-      template += `${indent}${lines}\n`
-      if (widgetType === 'set') {
-        indent += ONE_TAB
-      }
-    })
+  if (cardscript.actions) {
+    cardscript.actions.forEach(parseElement)
   }
 
+  /*
   if (cardscript.actions) {
     cardscript.actions.forEach(actionDefinition => {
       const label = `label="${actionDefinition.title}"`
