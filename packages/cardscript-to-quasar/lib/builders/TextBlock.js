02c8f2a8265f0d1be47c76cca30abc97083a515c
Meena.Hoda <meena.hoda@wmfs.net>
2018-12-06 09:46:51 +0000

    Beginning TextBlock quasar implementation

--- packages/cardscript-to-quasar/lib/builders/TextBlock.js
+++ packages/cardscript-to-quasar/lib/builders/TextBlock.js
@@ -1,10 +1,40 @@
 const ComponentBuilder = require('./../utils/Component-builder')
-// const GetAttribute = require('./../utils/Get-attribute')
 
 module.exports = function (definition, options) {
-  // const getAttribute = GetAttribute(widgetDefinition)
+  const {
+    color,
+    horizontalAlignment,
+    // isSubtle,
+    // maxLines,
+    // size,
+    text,
+    // weight,
+    // wrap,
+    id
+    // spacing,
+    // separator
+  } = definition
+
   const builder = new ComponentBuilder(definition)
-  const pre = builder.addTag('pre')
-  pre.content('// TODO: TextBlock!')
+  const div = builder.addTag('div')
+
+  if (id) div.addAttribute('id', id)
+
+  const classes = []
+
+  if (horizontalAlignment === 'left') classes.push('text-left')
+  if (horizontalAlignment === 'center') classes.push('text-center')
+  if (horizontalAlignment === 'right') classes.push('text-right')
+
+  if (color === 'primary') classes.push('text-primary')
+  if (color === 'good') classes.push('text-positive')
+  if (color === 'warning') classes.push('text-warning')
+  if (color === 'attention') classes.push('text-negative')
+  if (color === 'light') classes.push('text-light')
+  if (color === 'dark') classes.push('text-dark')
+
+  if (classes.length > 0) div.addAttribute('class', classes.join(' '))
+
+  div.content(text)
   return builder.compile()
 }
