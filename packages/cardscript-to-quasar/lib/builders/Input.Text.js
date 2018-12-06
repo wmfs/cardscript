19fb73221e5e28e0b52dee7fb40355283c053b53
Meena.Hoda <meena.hoda@wmfs.net>
2018-12-06 15:28:05 +0000

    Input.Text impl

--- packages/cardscript-to-quasar/lib/builders/Input.Text.js
+++ packages/cardscript-to-quasar/lib/builders/Input.Text.js
@@ -1,10 +1,40 @@
 const ComponentBuilder = require('./../utils/Component-builder')
-// const GetAttribute = require('./../utils/Get-attribute')
+
+const MARGINS = {
+  small: 'sm',
+  medium: 'md',
+  large: 'lg',
+  extraLarge: 'xl'
+}
 
 module.exports = function (definition, options) {
-  // const getAttribute = GetAttribute(widgetDefinition)
+  const {
+    isMultiline,
+    maxLength,
+    placeholder,
+    // style, // text, tel, url, email
+    spacing
+    // separator
+  } = definition
+
   const builder = new ComponentBuilder(definition)
-  const pre = builder.addTag('pre')
-  pre.content('// TODO: Input.Text!')
+
+  const input = builder.addTag('q-input')
+  input.bindToModel(definition)
+
+  if (placeholder) input.addAttribute('placeholder', placeholder)
+  if (maxLength) input.addAttribute('maxLength', maxLength)
+  if (isMultiline) input.addAttribute('type', 'textarea')
+
+  const classes = []
+
+  if (spacing === 'padding') {
+    classes.push('q-pa-md')
+  } else if (MARGINS[spacing]) {
+    classes.push(`q-mt-${MARGINS[spacing]}`)
+  }
+
+  input.addAttribute('class', classes.join(' '))
+
   return builder.compile()
 }
