5de283a65d7bc9a3463530ac2e91654139d59cb2
Meena.Hoda <meena.hoda@wmfs.net>
2018-12-06 14:40:05 +0000

    Image impl.

--- packages/cardscript-to-quasar/lib/builders/Image.js
+++ packages/cardscript-to-quasar/lib/builders/Image.js
@@ -1,10 +1,52 @@
 const ComponentBuilder = require('./../utils/Component-builder')
-// const GetAttribute = require('./../utils/Get-attribute')
+
+const SIZES = {
+  auto: '100%',
+  stretch: '100%',
+  small: '40px',
+  medium: '80px',
+  large: '120px'
+}
+
+const MARGINS = {
+  small: 'sm',
+  medium: 'md',
+  large: 'lg',
+  extraLarge: 'xl'
+}
 
 module.exports = function (definition, options) {
-  // const getAttribute = GetAttribute(widgetDefinition)
+  const {
+    altText,
+    // horizontalAlignment,
+    // selectAction,
+    size,
+    style,
+    url,
+    spacing,
+    // separator
+  } = definition
+
   const builder = new ComponentBuilder(definition)
-  const pre = builder.addTag('pre')
-  pre.content('// TODO: Image!')
+
+  const div = builder.addTag('div')
+  const img = div.addChildTag('img')
+
+  const classes = []
+
+  if (spacing === 'padding') {
+    classes.push('q-pa-md')
+  } else if (MARGINS[spacing]) {
+    classes.push(`q-mt-${MARGINS[spacing]}`)
+  }
+
+  if (classes.length > 0) div.addAttribute('class', classes.join(' '))
+
+  img.addAttribute('src', url)
+  img.addAttribute('alt', altText || 'Image')
+  img.addAttribute('width', SIZES[size] || '40px')
+
+  if (style === 'person') img.addAttribute('style', 'border-radius: 100%;')
+
   return builder.compile()
 }
