478fb7ae6b27766d56398ca992884f294df38037
Meena.Hoda <meena.hoda@wmfs.net>
2018-12-07 07:54:11 +0000

    Input.Number builder

--- packages/cardscript-to-quasar/lib/builders/Input.Number.js
+++ packages/cardscript-to-quasar/lib/builders/Input.Number.js
@@ -1,10 +1,44 @@
 const ComponentBuilder = require('./../utils/Component-builder')
-// const GetAttribute = require('./../utils/Get-attribute')
+
+const MARGINS = {
+  small: 'sm',
+  medium: 'md',
+  large: 'lg',
+  extraLarge: 'xl'
+}
 
 module.exports = function (definition, options) {
-  // const getAttribute = GetAttribute(widgetDefinition)
+  const {
+    max,
+    min,
+    maxLength,
+    placeholder,
+    spacing
+    // separator
+  } = definition
+
   const builder = new ComponentBuilder(definition)
-  const pre = builder.addTag('pre')
-  pre.content('// TODO: Input.Number!')
+
+  const input = builder.addTag('q-input')
+  input.bindToModel(definition)
+  input.addAttribute('type', 'number')
+
+  if (placeholder) input.addAttribute('placeholder', placeholder)
+  if (maxLength) input.addAttribute('maxLength', maxLength)
+
+  // Not sure if these are right...
+  if (min) input.addAttribute('min', min)
+  if (max) input.addAttribute('max', max)
+
+  const classes = []
+
+  if (spacing === 'padding') {
+    classes.push('q-pa-md')
+  } else if (MARGINS[spacing]) {
+    classes.push(`q-mt-${MARGINS[spacing]}`)
+  }
+
+  input.addAttribute('class', classes.join(' '))
+
   return builder.compile()
 }
