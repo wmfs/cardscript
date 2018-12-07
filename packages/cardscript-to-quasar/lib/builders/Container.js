5d950f64f4245e293d3adc717c476e8cc6dfedd1
Meena.Hoda <meena.hoda@wmfs.net>
2018-12-07 07:48:05 +0000

    Improve container styles

--- packages/cardscript-to-quasar/lib/builders/Container.js
+++ packages/cardscript-to-quasar/lib/builders/Container.js
@@ -7,6 +7,8 @@ const MARGINS = {
 
 module.exports = function (definition, options) {
   const {
+    backgroundImage,
+    wash,
     // selectAction,
     style,
     // verticalContentAlignment,
@@ -14,7 +16,7 @@ module.exports = function (definition, options) {
     // separator
   } = definition
 
-  let card = '<q-card '
+  let card = '<q-card'
 
   if (definition.hasOwnProperty('showWhen')) card += ` v-if="${definition.showWhen}"`
   if (definition.hasOwnProperty('id')) card += ` id="${definition.id}"`
@@ -30,8 +32,24 @@ module.exports = function (definition, options) {
     classes.push(`q-mt-${MARGINS[spacing]}`)
   }
 
-  if (classes.length > 0) card += `class="${classes.join(' ')}"`
+  if (classes.length > 0) card += ` class="${classes.join(' ')}"`
+
+  if (backgroundImage) {
+    const url = `url(statics/${backgroundImage})`
+    const blackWash = 'linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5))'
+    const whiteWash = 'linear-gradient(rgba(255,255,255,.5), rgba(255,255,255,.5))'
+
+    if (wash === 'black') {
+      card += ` style="background-image: ${blackWash}, ${url} !important;"`
+    } else if (wash === 'white') {
+      card += ` style="background-image: ${whiteWash}, ${url} !important;"`
+    } else {
+      card += ` style="background-image: ${url} !important;"`
+    }
+  }
 
   card += '> <q-card-main>'
+
+  console.log('>>>', card)
   return card
 }
