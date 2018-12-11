0004eecfd7bda08a05cdb2e22727d494bc673c0e
Meena.Hoda <meena.hoda@wmfs.net>
2018-12-11 12:44:52 +0000

    remove get-attribute

--- packages/cardscript-to-quasar/lib/builders/ImageSet.js
+++ packages/cardscript-to-quasar/lib/builders/ImageSet.js
@@ -1,8 +1,6 @@
 const ComponentBuilder = require('./../utils/Component-builder')
-// const GetAttribute = require('./../utils/Get-attribute')
 
 module.exports = function (definition, options) {
-  // const getAttribute = GetAttribute(widgetDefinition)
   const builder = new ComponentBuilder(definition)
   const pre = builder.addTag('pre')
   pre.content('// TODO: ImageSet!')
