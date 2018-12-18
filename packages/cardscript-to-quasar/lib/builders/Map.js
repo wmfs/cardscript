723b55fb9e90b25d6ff2a691ed7b3ccc8d8437c9
Meena.Hoda <meena.hoda@wmfs.net>
2018-12-18 08:16:56 +0000

    Added skeleton for AddressBlock, Map and PhaseBanner

---
+++ packages/cardscript-to-quasar/lib/builders/Map.js
@@ -0,0 +1,8 @@
+const ComponentBuilder = require('./../utils/Component-builder')
+
+module.exports = function (definition, options) {
+  const builder = new ComponentBuilder(definition)
+  const pre = builder.addTag('pre')
+  pre.content('// TODO: Map!')
+  return builder.compile()
+}
