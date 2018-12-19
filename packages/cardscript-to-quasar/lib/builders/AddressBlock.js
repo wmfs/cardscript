4bc0cf238624fb700be776872ddc0b2b62c99720
Meena.Hoda <meena.hoda@wmfs.net>
2018-12-19 11:51:26 +0000

    Basic AddressBlock implementation

--- packages/cardscript-to-quasar/lib/builders/AddressBlock.js
+++ packages/cardscript-to-quasar/lib/builders/AddressBlock.js
@@ -1,8 +1,29 @@
 const ComponentBuilder = require('./../utils/Component-builder')
 
 module.exports = function (definition, options) {
+  const {
+    multiline,
+    lineDelimited,
+    title,
+    dataPath
+  } = definition
   const builder = new ComponentBuilder(definition)
-  const pre = builder.addTag('pre')
-  pre.content('// TODO: AddressBlock!')
+
+  const div = builder.addTag('div')
+
+  const titleText = div.addChildTag('p')
+  titleText.content(title || 'Address')
+
+  const notFoundText = div.addChildTag('p')
+  notFoundText.content('Address not found.')
+  notFoundText.addAttribute('v-if', `!data.${dataPath}`)
+
+  const lines = div.addChildTag('div')
+  lines.addAttribute('v-if', `data.${dataPath}`)
+  const line = lines.addChildTag('p')
+  line.addAttribute('v-for', `(line, $idx) in data.${dataPath}.split('${lineDelimited || ','}')`)
+  line.addAttribute(':key', '$idx')
+  line.content(`{{line}}`)
+
   return builder.compile()
 }
