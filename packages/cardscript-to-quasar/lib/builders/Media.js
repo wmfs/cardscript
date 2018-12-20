c3ae32883b23f9b969a260c7c130fee86b13699a
Meena.Hoda <meena.hoda@wmfs.net>
2018-12-20 09:49:06 +0000

    Improve height on Media

--- packages/cardscript-to-quasar/lib/builders/Media.js
+++ packages/cardscript-to-quasar/lib/builders/Media.js
@@ -8,6 +8,8 @@ module.exports = function (definition, options) {
     muted: false,
     language: 'en',
     playbackRates: [0.7, 1.0, 1.5, 2.0],
+    height: '500px',
+    width: '100%',
     sources: sources.map(({ mimeType, url }) => {
       return {
         type: mimeType,
@@ -19,7 +21,7 @@ module.exports = function (definition, options) {
   const builder = new ComponentBuilder(definition)
 
   const videoPlayer = builder.addTag('video-player')
-  videoPlayer.addAttribute('class', 'video-player-box')
+  videoPlayer.addAttribute('class', 'video-player-box q-mt-md')
   videoPlayer.addAttribute('ref', 'videoPlayer')
   videoPlayer.addAttribute(':options', inspect(opts))
 
