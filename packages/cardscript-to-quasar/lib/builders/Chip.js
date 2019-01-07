343cf11aec3cbc7d4ddb7a62e31c879367b358f1
Meena.Hoda <meena.hoda@wmfs.net>
2019-01-07 09:11:02 +0000

    Add spacing attrib to Chip

--- packages/cardscript-to-quasar/lib/builders/Chip.js
+++ packages/cardscript-to-quasar/lib/builders/Chip.js
@@ -9,16 +9,40 @@ const COLORS = {
   dark: 'dark'
 }
 
+const MARGINS = {
+  small: 'sm',
+  medium: 'md',
+  large: 'lg',
+  extraLarge: 'xl'
+}
+
 module.exports = function (definition, options) {
-  const { icon, color, text } = definition
+  const {
+    icon,
+    color,
+    text,
+    spacing
+  } = definition
 
   const builder = new ComponentBuilder(definition)
 
-  const chip = builder.addTag('q-chip')
+  const classes = []
+
+  const div = builder.addTag('div')
+  const chip = div.addChildTag('q-chip')
   chip.content(text)
 
   if (icon) chip.addAttribute('icon', icon)
+
   if (color && COLORS[color]) chip.addAttribute('color', COLORS[color])
+
+  if (spacing === 'padding') {
+    classes.push(`q-pa-md`)
+  } else if (spacing && MARGINS[spacing]) {
+    classes.push(`q-mt-${MARGINS[spacing]}`)
+  }
+
+  if (classes.length > 0) div.addAttribute('class', classes.join(' '))
 
   return builder.compile()
 }
