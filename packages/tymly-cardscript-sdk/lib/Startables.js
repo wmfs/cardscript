23fa686219d5f5f39b81e38dde5274846a37238d
Meena.Hoda <meena.hoda@wmfs.net>
2019-01-16 09:02:02 +0000

    sdk refactor getting favourites to make a bit simpler

--- packages/tymly-cardscript-sdk/lib/Startables.js
+++ packages/tymly-cardscript-sdk/lib/Startables.js
@@ -21,28 +21,28 @@ module.exports = class Startables {
     const startables = await this.db.startables.toArray()
     this.store.commit('app/startables', startables)
 
-    const favourites = await this.db.favourites.toArray()
-    this.store.commit('app/favourites', favourites[0].favourites)
+    const { favourites } = await this.db.favourites.get('favourites')
+    this.store.commit('app/favourites', favourites)
   }
 
   // todo: probably refactor these
   async favourite (id) {
     this.store.commit('app/favourite', id)
 
-    const favourites = await this.db.favourites.toArray()
-    if (!favourites[0].favourites.includes(id)) favourites[0].favourites.push(id)
+    const { favourites } = await this.db.favourites.get('favourites')
+    if (!favourites.includes(id)) favourites.push(id)
 
-    await this.db.favourites.put(favourites[0])
+    await this.db.favourites.put({ id: 'favourites', favourites })
   }
 
   async unfavourite (id) {
     this.store.commit('app/unfavourite', id)
 
-    const favourites = await this.db.favourites.toArray()
+    const { favourites } = await this.db.favourites.get('favourites')
 
-    const index = favourites[0].favourites.indexOf(id)
-    if (index > -1) favourites[0].favourites.splice(index, 1)
+    const index = favourites.indexOf(id)
+    if (index > -1) favourites.splice(index, 1)
 
-    await this.db.favourites.put(favourites[0])
+    await this.db.favourites.put({ id: 'favourites', favourites })
   }
 }
