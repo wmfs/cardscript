35aa42fd573da37bc2fae40c78f2673caddb721f
Meena.Hoda <meena.hoda@wmfs.net>
2019-05-24 11:02:30 +0100

    refactor: upgrade components to quasar v1

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -63,163 +63,167 @@
         </div>
 
         <div v-if="validation.state === 'valid'">
-          <transition
-            appear
-            enter-active-class="animated fadeIn"
-            leave-active-class="animated fadeOut"
+          <q-tabs
+            v-model="tabIndex"
+            align="justify"
+            class="text-grey"
+            active-color="primary"
+            indicator-color="primary"
+            narrow-indicator
+            dense
           >
-            <q-tabs no-pane-border inverted class="q-pt-md" align="justify">
-              <q-tab default slot="title" name="card-tab" label="card"></q-tab>
-              <q-tab slot="title" name="model-tab" label="model"></q-tab>
-              <q-tab slot="title" name="template-tab" label="template"></q-tab>
-              <q-tab slot="title" name="info-tab" label="info"></q-tab>
-              <q-tab-pane name="card-tab" class="tab-pane">
-                <blockquote>
-                  This is a simple rendering of the parsed Cardscript. Note that this is only meant to be a basic
-                  illustration of typical web usage, your app is free to interpret Cardscript and conjure a UI in
-                  any way
-                  you see fit!
-                </blockquote>
+            <q-tab name="card-tab" label="Card"/>
+            <q-tab name="model-tab" label="Model"/>
+            <q-tab name="template-tab" label="Template"/>
+            <q-tab name="info-tab" label="Info"/>
+          </q-tabs>
 
-                <q-list
-                  v-if="dynamicContent.toc.length > 0"
-                  highlight
-                  class="q-mt-md"
+          <q-tab-panels v-model="tabIndex">
+            <q-tab-panel name="card-tab">
+              <blockquote>
+                This is a simple rendering of the parsed Cardscript. Note that this is only meant to be a basic
+                illustration of typical web usage, your app is free to interpret Cardscript and conjure a UI in
+                any way
+                you see fit!
+              </blockquote>
+
+              <q-list
+                v-if="dynamicContent.toc.length > 0"
+                highlight
+                class="q-mt-md"
+              >
+                <q-item
+                  v-for="entry in dynamicContent.toc"
+                  :key="entry.elementId"
+                  @click.native="tocClick(entry.elementId)"
+                  class="cursor-pointer"
                 >
-                  <q-item
-                    v-for="entry in dynamicContent.toc"
-                    :key="entry.elementId"
-                    @click.native="tocClick(entry.elementId)"
-                    class="cursor-pointer"
-                  >
-                    <q-item-section side left>
-                      <q-icon :name="entry.tocIcon" />
-                    </q-item-section>
-                    <q-item-section>
-                      <q-item-label>{{entry.tocTitle}}</q-item-label>
-                    </q-item-section>
-                  </q-item>
-                </q-list>
+                  <q-item-section side left>
+                    <q-icon :name="entry.tocIcon"/>
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>{{entry.tocTitle}}</q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
 
-                <q-card class="q-mt-md q-mb-xl">
-                  <q-card-section>
-                    <cardscript
-                      :content="dynamicContent"
-                      @OpenURL="onOpenURL"
-                      @Submit="onSubmit"
-                      @ShowCard="onShowCard"
-                      @InputAddress="onInputAddress"
-                      @InputApiLookup="onInputApiLookup"
-                    />
-                  </q-card-section>
-                </q-card>
+              <q-card class="q-mt-md q-mb-xl">
+                <q-card-section>
+                  <cardscript
+                    :content="dynamicContent"
+                    @OpenURL="onOpenURL"
+                    @Submit="onSubmit"
+                    @ShowCard="onShowCard"
+                    @InputAddress="onInputAddress"
+                    @InputApiLookup="onInputApiLookup"
+                  />
+                </q-card-section>
+              </q-card>
+            </q-tab-panel>
+            <q-tab-panel name="model-tab">
+              <blockquote>
+                This is the underlying data model for the card (default values were inferred from the Cardscript
+                using the
+                <a href="https://github.com/wmfs/cardscript-extract-defaults">cardscript-extract-defaults</a>
+                package).
+                Be sure to check back here as you change input fields to see the model change!
+              </blockquote>
 
-              </q-tab-pane>
-              <q-tab-pane name="model-tab" class="tab-pane">
-                <blockquote>
-                  This is the underlying data model for the card (default values were inferred from the Cardscript
-                  using the
-                  <a href="https://github.com/wmfs/cardscript-extract-defaults">cardscript-extract-defaults</a>
-                  package).
-                  Be sure to check back here as you change input fields to see the model change!
-                </blockquote>
+              <q-input
+                type="textarea"
+                readonly
+                v-bind:value="dynamicContent.data | pretty"
+                :max-height="300"
+                rows="10"
+                class="q-mt-md q-pa-sm bg-dark code"
+                dark
+              />
+            </q-tab-panel>
+            <q-tab-panel name="template-tab">
+              <blockquote>
+                The content below has been produced using the <a
+                href="https://github.com/wmfs/cardscript-to-quasar">cardscript-to-quasar</a>
+                and
+                <a href="https://github.com/wmfs/cardscript-extract-lists">cardscript-extract-lists</a>
+                packages. Here we've configured things to output in a Vue.js style, but Angular and React
+                templates
+                can
+                be generated too!
+              </blockquote>
 
-                <q-input
-                  type="textarea"
-                  readonly
-                  v-bind:value="dynamicContent.data | pretty"
-                  :max-height="300"
-                  rows="10"
-                  class="q-mt-md q-pa-sm bg-dark code"
-                  dark
-                />
-              </q-tab-pane>
-              <q-tab-pane name="template-tab" class="tab-pane">
-                <blockquote>
-                  The content below has been produced using the <a
-                  href="https://github.com/wmfs/cardscript-to-quasar">cardscript-to-quasar</a>
-                  and
-                  <a href="https://github.com/wmfs/cardscript-extract-lists">cardscript-extract-lists</a>
-                  packages. Here we've configured things to output in a Vue.js style, but Angular and React
-                  templates
-                  can
-                  be generated too!
-                </blockquote>
+              <div class="text-h4 q-my-md">Quasar Template</div>
+              <q-input
+                type="textarea"
+                readonly
+                v-bind:value="dynamicContent.quasarTemplate"
+                :max-height="300"
+                rows="10"
+                class="q-mt-md q-pa-sm bg-dark code"
+                dark
+              />
 
-                <div class="text-h4 q-my-md">Quasar Template</div>
-                <q-input
-                  type="textarea"
-                  readonly
-                  v-bind:value="dynamicContent.quasarTemplate"
-                  :max-height="300"
-                  rows="10"
-                  class="q-mt-md q-pa-sm bg-dark code"
-                  dark
-                />
+              <div class="text-h4 q-my-md">Lists</div>
+              <q-input
+                type="textarea"
+                readonly
+                v-bind:value="dynamicContent.lists | pretty"
+                :max-height="300"
+                rows="10"
+                class="q-mt-md q-pa-sm bg-dark code"
+                dark
+              />
+            </q-tab-panel>
+            <q-tab-panel name="info-tab">
+              <div class="text-h4 q-my-md">Performance</div>
+              <p>
+                This playpen is working with raw Cardscript (parsing, validating and transforming).
+                Most apps wouldn't need to do that kind of heavy lifting in the client.
+                As such, the rendering times inside the playpen are higher than usual... this is where all the
+                time just went:
+              </p>
+              <q-list highlight>
+                <q-item
+                  v-for="(time, idx) in dynamicContent.times"
+                  :key="idx"
+                >
+                  <q-item-section>
+                    <q-item-label>
+                      {{time.label}}
+                    </q-item-label>
+                  </q-item-section>
+                  <q-item-section side right>
+                    <q-item-label>{{time.duration}}ms</q-item-label>
+                    <q-item-label>{{time.percentage}}%</q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-item-separator/>
+                <q-item>
+                  <q-item-section>
+                    <q-item-label>
+                      Total
+                    </q-item-label>
+                  </q-item-section>
+                  <q-item-section side right>
+                    <q-item-label>{{dynamicContent.totalTime}}ms</q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
 
-                <div class="text-h4 q-my-md">Lists</div>
-                <q-input
-                  type="textarea"
-                  readonly
-                  v-bind:value="dynamicContent.lists | pretty"
-                  :max-height="300"
-                  rows="10"
-                  class="q-mt-md q-pa-sm bg-dark code"
-                  dark
-                />
-              </q-tab-pane>
-              <q-tab-pane name="info-tab" class="tab-pane">
-                <div class="text-h4 q-my-md">Performance</div>
-                <p>
-                  This playpen is working with raw Cardscript (parsing, validating and transforming).
-                  Most apps wouldn't need to do that kind of heavy lifting in the client.
-                  As such, the rendering times inside the playpen are higher than usual... this is where all the
-                  time just went:
-                </p>
-                <q-list highlight>
-                  <q-item
-                    v-for="(time, idx) in dynamicContent.times"
-                    :key="idx"
-                  >
-                    <q-item-section>
-                      <q-item-label>
-                        {{time.label}}
-                      </q-item-label>
-                    </q-item-section>
-                    <q-item-section side right>
-                      <q-item-label>{{time.duration}}ms</q-item-label>
-                      <q-item-label>{{time.percentage}}%</q-item-label>
-                    </q-item-section>
-                  </q-item>
-                  <q-item-separator/>
-                  <q-item>
-                    <q-item-section>
-                      <q-item-label>
-                        Total
-                      </q-item-label>
-                    </q-item-section>
-                    <q-item-section side right>
-                      <q-item-label>{{dynamicContent.totalTime}}ms</q-item-label>
-                    </q-item-section>
-                  </q-item>
-                </q-list>
-
-                <div class="text-h4 q-my-md">Internals</div>
-                <p>
-                  Here are some of the internal workings (for managing dialog states and card lists especially)
-                </p>
-                <q-input
-                  type="textarea"
-                  readonly
-                  v-bind:value="dynamicContent.internals | pretty"
-                  :max-height="300"
-                  rows="10"
-                  class="q-mt-md q-pa-sm bg-dark code"
-                  dark
-                />
-              </q-tab-pane>
-            </q-tabs>
-          </transition>
+              <div class="text-h4 q-my-md">Internals</div>
+              <p>
+                Here are some of the internal workings (for managing dialog states and card lists especially)
+              </p>
+              <q-input
+                type="textarea"
+                readonly
+                v-bind:value="dynamicContent.internals | pretty"
+                :max-height="300"
+                rows="10"
+                class="q-mt-md q-pa-sm bg-dark code"
+                dark
+              />
+            </q-tab-panel>
+          </q-tab-panels>
         </div>
       </div>
     </div>
@@ -336,6 +340,7 @@
     },
     data: function () {
       return {
+        tabIndex: 'card-tab',
         dynamicContent: getEmptyDynamicContent(),
         validation: {
           state: 'notValidated',
@@ -368,9 +373,9 @@
         const query = dottie.get(that, `${payload.dataPath}.${payload.id}SearchFld`)
         if (query && query.trim().length > 0) {
           dottie.set(that, `${payload.dataPath}.${payload.id}SearchResults`, [
-            { label: '1 Red Road ', value: '1' },
-            { label: '2 Red Road ', value: '2' },
-            { label: '3 Red Road ', value: '3' }
+            {label: '1 Red Road ', value: '1'},
+            {label: '2 Red Road ', value: '2'},
+            {label: '3 Red Road ', value: '3'}
           ])
         }
       },
